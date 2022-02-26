096e10895b5380166329c5c6ed7b228d3b3a6206
vincenttuan <vincentjava@yahoo.com.tw>
2022-02-26 10:26:54 +0800

    spring

--- target/classes/com/study/springmvc/lab/lab.sql
+++ target/classes/com/study/springmvc/lab/lab.sql
@@ -13,3 +13,42 @@ create table if not exists fundstock (
 	foreign key(fid) references fund(fid), -- 外鍵關聯
 	primary key(sid)
 );
+
+insert into fund(fname) values ('A');
+insert into fund(fname) values ('B');
+insert into fund(fname) values ('C');
+insert into fund(fname) values ('D');
+insert into fund(fname) values ('E');
+insert into fund(fname) values ('F');
+insert into fund(fname) values ('G');
+
+insert into fundstock(fid, symbol, share) values (1, "2330.TW", 50000);
+insert into fundstock(fid, symbol, share) values (2, "2330.TW", 60000);
+insert into fundstock(fid, symbol, share) values (2, "2303.TW", 70000);
+insert into fundstock(fid, symbol, share) values (3, "2376.TW", 80000);
+insert into fundstock(fid, symbol, share) values (3, "1101.TW", 90000);
+insert into fundstock(fid, symbol, share) values (3, "2317.TW", 80000);
+insert into fundstock(fid, symbol, share) values (4, "3231.TW", 70000);
+insert into fundstock(fid, symbol, share) values (4, "2356.TW", 60000);
+insert into fundstock(fid, symbol, share) values (4, "2880.TW", 50000);
+insert into fundstock(fid, symbol, share) values (4, "2891.TW", 40000);
+insert into fundstock(fid, symbol, share) values (5, "2330.TW", 30000);
+insert into fundstock(fid, symbol, share) values (5, "1101.TW", 20000);
+insert into fundstock(fid, symbol, share) values (5, "2317.TW", 10000);
+insert into fundstock(fid, symbol, share) values (5, "2886.TW", 20000);
+insert into fundstock(fid, symbol, share) values (5, "2002.TW", 30000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
